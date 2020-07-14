b825f82f53e2349cafcd9c87070bb46bce3bdc4c
kalucky0 <kalucki23@gmail.com>
2020-07-14 13:40:31 +0200

    Moved materials array to separate file

---
+++ materials.js
@@ -0,0 +1,181 @@
+const materials = [{
+    name: "PBR PLASTIC",
+    img: ["pbrplastic.png"],
+    file: "PBRPlastic.blend",
+    size: 6.89,
+    blender: "2.83.2",
+    author: "",
+    pub: "July 13, 2020",
+    lic: "CC0 1.0 Universal"
+}, {
+    name: "CUTTINGMAT",
+    img: ["cuttingmat.png"],
+    file: "CuttingMat.blend",
+    size: 6.93,
+    author: "",
+    blender: "2.83.2",
+    pub: "July 13, 2020",
+    lic: "CC0 1.0 Universal"
+}, {
+    name: "FLANNEL",
+    img: ["flannel.png"],
+    file: "Flannel.blend",
+    size: 7.00,
+    author: "",
+    blender: "2.83.2",
+    pub: "July 13, 2020",
+    lic: "CC0 1.0 Universal"
+}, {
+    name: "DEFAULT",
+    img: ["default.png"],
+    file: "",
+    size: 0.00,
+    author: "",
+    blender: "0.0",
+    pub: "Januray 1, 1970",
+    lic: ""
+}, {
+    name: "DEFAULT",
+    img: ["default.png"],
+    file: "",
+    size: 0.00,
+    author: "",
+    blender: "0.0",
+    pub: "Januray 1, 1970",
+    lic: ""
+}, {
+    name: "DEFAULT",
+    img: ["default.png"],
+    file: "",
+    size: 0.00,
+    author: "",
+    blender: "0.0",
+    pub: "Januray 1, 1970",
+    lic: ""
+}, {
+    name: "DEFAULT",
+    img: ["default.png"],
+    file: "",
+    size: 0.00,
+    author: "",
+    blender: "0.0",
+    pub: "Januray 1, 1970",
+    lic: ""
+}, {
+    name: "DEFAULT",
+    img: ["default.png"],
+    file: "",
+    size: 0.00,
+    author: "",
+    blender: "0.0",
+    pub: "Januray 1, 1970",
+    lic: ""
+}, {
+    name: "DEFAULT",
+    img: ["default.png"],
+    file: "",
+    size: 0.00,
+    author: "",
+    blender: "0.0",
+    pub: "Januray 1, 1970",
+    lic: ""
+}, {
+    name: "DEFAULT",
+    img: ["default.png"],
+    file: "",
+    size: 0.00,
+    author: "",
+    blender: "0.0",
+    pub: "Januray 1, 1970",
+    lic: ""
+}, {
+    name: "DEFAULT",
+    img: ["default.png"],
+    file: "",
+    size: 0.00,
+    author: "",
+    blender: "0.0",
+    pub: "Januray 1, 1970",
+    lic: ""
+}, {
+    name: "DEFAULT",
+    img: ["default.png"],
+    file: "",
+    size: 0.00,
+    author: "",
+    blender: "0.0",
+    pub: "Januray 1, 1970",
+    lic: ""
+}, {
+    name: "DEFAULT",
+    img: ["default.png"],
+    file: "",
+    size: 0.00,
+    author: "",
+    blender: "0.0",
+    pub: "Januray 1, 1970",
+    lic: ""
+}, {
+    name: "DEFAULT",
+    img: ["default.png"],
+    file: "",
+    size: 0.00,
+    author: "",
+    blender: "0.0",
+    pub: "Januray 1, 1970",
+    lic: ""
+}, {
+    name: "DEFAULT",
+    img: ["default.png"],
+    file: "",
+    size: 0.00,
+    author: "",
+    blender: "0.0",
+    pub: "Januray 1, 1970",
+    lic: ""
+}, {
+    name: "DEFAULT",
+    img: ["default.png"],
+    file: "",
+    size: 0.00,
+    author: "",
+    blender: "0.0",
+    pub: "Januray 1, 1970",
+    lic: ""
+}, {
+    name: "DEFAULT",
+    img: ["default.png"],
+    file: "",
+    size: 0.00,
+    author: "",
+    blender: "0.0",
+    pub: "Januray 1, 1970",
+    lic: ""
+}, {
+    name: "DEFAULT",
+    img: ["default.png"],
+    file: "",
+    size: 0.00,
+    author: "",
+    blender: "0.0",
+    pub: "Januray 1, 1970",
+    lic: ""
+}, {
+    name: "DEFAULT",
+    img: ["default.png"],
+    file: "",
+    size: 0.00,
+    author: "",
+    blender: "0.0",
+    pub: "Januray 1, 1970",
+    lic: ""
+}, {
+    name: "DEFAULT",
+    img: ["default.png"],
+    file: "",
+    size: 0.00,
+    author: "",
+    blender: "0.0",
+    pub: "Januray 1, 1970",
+    lic: ""
+}];
--- script.js
+++ script.js
@@ -1,99 +1,5 @@
-const materials = [{
-    name: "PBR PLASTIC",
-    img: ["pbrplastic.png"],
-    file: "PBRPlastic.blend",
-}, {
-    name: "CUTTINGMAT",
-    img: ["cuttingmat.png"],
-    file: "CuttingMat.blend"
-}, {
-    name: "FLANNEL",
-    img: ["flannel.png"],
-    file: "Flannel.blend"
-}, {
-    name: "DEFAULT",
-    img: ["default.png"],
-    file: ""
-}, {
-    name: "DEFAULT",
-    img: ["default.png"],
-    file: ""
-}, {
-    name: "DEFAULT",
-    img: ["default.png"],
-    file: ""
-}, {
-    name: "DEFAULT",
-    img: ["default.png"],
-    file: ""
-}, {
-    name: "DEFAULT",
-    img: ["default.png"],
-    file: ""
-}, {
-    name: "DEFAULT",
-    img: ["default.png"],
-    file: ""
-}, {
-    name: "DEFAULT",
-    img: ["default.png"],
-    file: ""
-},
-{
-    name: "DEFAULT",
-    img: ["default.png"],
-    file: ""
-},
-{
-    name: "DEFAULT",
-    img: ["default.png"],
-    file: ""
-},
-{
-    name: "DEFAULT",
-    img: ["default.png"],
-    file: ""
-},
-{
-    name: "DEFAULT",
-    img: ["default.png"],
-    file: ""
-},
-{
-    name: "DEFAULT",
-    img: ["default.png"],
-    file: ""
-},
-{
-    name: "DEFAULT",
-    img: ["default.png"],
-    file: ""
-},
-{
-    name: "DEFAULT",
-    img: ["default.png"],
-    file: ""
-},
-{
-    name: "DEFAULT",
-    img: ["default.png"],
-    file: ""
-},
-{
-    name: "DEFAULT",
-    img: ["default.png"],
-    file: ""
-},
-{
-    name: "DEFAULT",
-    img: ["default.png"],
-    file: ""
-},
-];
-
 const row = document.getElementById("materials");
-
-for (let i = 0; i < materials.length; i++) {
+for (let i = 0; i < materials.length; i += 1) {
     row.innerHTML += `<div class="column">
             <div class="card">
                 <img src="img/materials/${materials[i].img[0]}" alt="${materials[i].name}" style="width:100%;border-radius: 10px 10px 0 0;">
@@ -101,26 +7,28 @@ for (let i = 0; i < materials.length; i++) {
                     ${materials[i].name}
                 </div>
             </div>
-        </div>`;
+        </div>`
 }
-
 const modal = document.getElementById("modal");
 const cards = document.querySelectorAll(".card");
 const span = document.getElementsByClassName("close")[0];
-
-for(let i = 0; i < cards.length; i++) {
+for (let i = 0; i < cards.length; i += 1) {
     cards[i].onclick = function () {
         modal.style.display = "block";
         modal.querySelector("h2").innerHTML = materials[i].name;
         modal.querySelector("img").src = "img/materials/" + materials[i].img[0];
+        modal.querySelectorAll("span")[1].innerText = materials[i].lic
+        modal.querySelectorAll("span")[2].innerText = materials[i].author
+        modal.querySelectorAll("span")[3].innerText = materials[i].blender
+        modal.querySelectorAll("span")[4].innerText = materials[i].size + " MB"
+        modal.querySelectorAll("span")[5].innerText = materials[i].pub
     }
 }
-
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target == modal)
-        modal.style.display = "none";
+span.onclick = function () {
+    modal.style.display = "none"
+};
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none"
+    }
 }
